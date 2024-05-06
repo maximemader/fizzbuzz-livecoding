2297059095a514f25ef267a2b32725e46da8c3f0
Maxime MADER <maxime.mader@carbon-it.com>
2024-05-06 10:17:49 +0200

    Version bump

---
+++ fizzbuzz-livecoding/src/main/java/Program.java
@@ -0,0 +1,11 @@
+public class Program {
+    public static void main(String[] args) {
+        /*
+            - Write a program that prints one line for each number from 1 to 100. But,
+            - for multiples of three print Fizz instead of the number,
+            - for multiples of five print Buzz instead of the number,
+            - for numbers which are multiples of both three and five print FizzBuzz instead of the number.
+         */
+        System.out.println("Hello, World!");
+    }
+}
